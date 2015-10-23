3ace43de4eb82016bb4960aaff9f94db40018ad4
Clay Miller <clay@smockle.com>
2015-10-22 22:24:27 -0700

    Add cask for premiere-webm.

---
+++ premiere-webm.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'photoshop-webm' do
+  version '1.0.1'
+  sha256 '68a2d856d545a4b7e3ccc39f5cbf635ed97d33322eac494b121c40bb32a69688'
+
+  url 'http://www.fnordware.com/downloads/WebM_Premiere_v1.0.1.dmg'
+  name 'WebM for Premiere'
+  homepage 'http://www.fnordware.com/WebM/'
+  license :oss
+
+  pkg 'WebM Premiere.mpkg'
+  uninstall 'com.fnordware.*'
+end
