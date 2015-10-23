5d1ed27cdedd79193018668165820b8f1da44e90
Clay Miller <clay@smockle.com>
2015-10-22 20:04:09 -0700

    Update jpegxr

--- jpegxr.rb
+++ jpegxr.rb
@@ -6,14 +6,15 @@ class Jpegxr < Formula
   sha256 "ede372eb29ad9c56f6450cda459548aca15febaa9ae8f4857d523814583e34a2"
 
   def install
-    system "ls", buildpath
-    system "ls", prefix
-    system "ls", "/Library/Caches/Homebrew"
-    # system "unzip", buildpath/"jpegxr-#{version}.zip"
-    # system "cp", "-Rf", buildpath/"JPEGXR_Mac.plugin", "/Applications/Adobe\ Photoshop\ CC\ 2015/Plug-ins/File\ Formats/"
+    doc.install Dir["#{buildpath}/*.rtf"]
+    lib.install "JPEGXR.plugin"
+    if File.exist? "/Applications/Adobe\ Photoshop\ CC\ 2015/Plug-ins/File\ Formats/JPEGXR.plugin"
+      system "unlink", "/Applications/Adobe\ Photoshop\ CC\ 2015/Plug-ins/File\ Formats/JPEGXR.plugin"
+    end
+    system "ln", "-s", lib/"JPEGXR.plugin", "/Applications/Adobe\ Photoshop\ CC\ 2015/Plug-ins/File\ Formats"
   end
 
   test do
-    File.exist? "/Applications/Adobe\ Photoshop\ CC\ 2015/Plug-ins/File\ Formats/JPEGXR_Mac.plugin"
+    File.exist? "/Applications/Adobe\ Photoshop\ CC\ 2015/Plug-ins/File\ Formats/JPEGXR.plugin"
   end
 end
